cce330c75d51de3b0dd736b6920fcd8a3ce186e0
LENOVO <s@ul>
2026-08-15 23:31:07 -0500

    Probar aplicación en Render

--- fiel/src/main/java/com/troll/fiel/FielController.java
+++ fiel/src/main/java/com/troll/fiel/FielController.java
@@ -1,13 +1,13 @@
 package com.troll.fiel;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FielController {
 
     @GetMapping("/")
     public String inicio() {
-        return "fiel";
+        return "La aplicación está funcionando correctamente";
     }
 }
